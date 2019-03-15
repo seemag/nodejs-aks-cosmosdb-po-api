fd3d3d3bebacdd9a7be1f088cff9f357e52d1221
segupta <segupta@microsoft.com>
2019-03-15 23:12:53 +0800

    cosmosdb add

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 let config = {}
 
 // Configure the host and authkey before deploying this app!!
-config.host = process.env.HOST || "https://vcs-cosmosdb.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "1pxILrCIRPhunE5p9R6tA4Y5ZrqJGyNiCKD3LBh3zUSAADMuoaMa0AtdHVujR8ysWDAGTsyRtwqibLoSlnwt0A==";
+config.host = process.env.HOST || "https://nodeakscosmosdb.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "rE2NlwUHxiXSvYahegMjPxsMnk5lSvy573sci4c5sws77T3pgVifjo7mZjXr6rgtUqTOJCLvirqeHaSOtbyrRg==";
 config.databaseId = "OrderDB";
 config.collectionId = "PurchaseOrders";
-
+	
 module.exports = config;
